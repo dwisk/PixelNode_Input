accf856ab5fa9aaae5609aab5f175d74d86961b5
Amely Kling <mail@dwi.sk>
2015-05-24 18:32:30 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * PixelNode_Input 
+ * 
+ * Base class for input sources
+ * 
+ * --------------------------------------------------------------------------------------------------------------------
+ * 
+ * @author Amely Kling <mail@dwi.sk>
+ *
+ */
+
+
+/* Node Inclues
+ * ==================================================================================================================== */
+
+var _ = require('underscore');
+
+
+/* Class Constructor
+ * ==================================================================================================================== */
+
+function PixelNode_Input(options) {	
+	this.options = _.extend({},this.base_options, this.default_options, options);
+	this.init();
+}
+
+// module export
+module.exports = PixelNode_Input;
+
+
+/* Variables
+ * ==================================================================================================================== */
+
+PixelNode_Input.prototype.base_options = {}
+PixelNode_Input.prototype.default_options = {}
+PixelNode_Input.prototype.options = {}
+
+
+/* Methods
+ * ==================================================================================================================== */
+
+// init input – override
+PixelNode_Input.prototype.init = function() {
+	console.log("Init Input".grey);
+}
+
+// sets input via PixelNode_Data
+PixelNode_Input.prototype.setInputData = function(data, target) {
+	if (target) {		
+		global.pixelNode.data.set(["inputs",self.options.name, target], data);
+	} else {
+		global.pixelNode.data.set(["inputs",self.options.name], data);
+	}
+}
